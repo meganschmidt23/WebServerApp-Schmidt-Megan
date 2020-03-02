b3ac3045fcefa69994142adc7269b860f48f8800
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-03-01 21:10:06 -0500

    Added components of Fitness App

---
+++ client/src/models/FitnessApp.js
@@ -0,0 +1,11 @@
+export constant User = [
+     {Name: 'Bob', Weight: 230, Gender: 'Male'}
+];
+
+export constant MyWorkouts = [];
+
+export const TotalWorkouts = [];
+//Add new workout to your total workouts
+export function ExerciseInit(){
+    MyWorkouts.push(TotalWorkouts[0])
+}
