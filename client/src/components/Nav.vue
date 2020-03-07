3cf7eaf5fb1a9d9cf4fdd8159553d0ef0c5d1e38
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-03-07 13:28:17 -0500

    Updating client

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,30 +1,26 @@
 <template>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
     </a>
 
-    <a :class="{'is-active':isOpen}" @click="isOpen = !isOpen" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div :class="{'is-active':isOpen}" class="navbar-menu">
+  <div :class="{ 'is-active':isOpen }" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
 
-      <a class="navbar-item">
-        Documentation
-      </a>
-   
-        <router-link to="/" class="navbar-item">Home</router-link> |
-        <router-link to="/about" class="navbar-item">About</router-link>
-   
+
+      <router-link to="/" class="navbar-item" active-class="is-current" exact>Home</router-link>
+      <router-link to="/about" class="navbar-item" active-class="is-current">About</router-link>
+      <router-link to="/game" class="navbar-item" active-class="is-current">Login</router-link>
+
+
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           More
@@ -54,7 +50,7 @@
           <a class="button is-primary">
             <strong>Sign up</strong>
           </a>
-          <a class="button is-light">
+          <a class="button is-light" href="/login">
             Log in
           </a>
         </div>
@@ -63,14 +59,18 @@
   </div>
 </nav>
 </template>
+
 <script>
 export default {
-    data: ()=>({
-        isOpen: false
-    })
+  data: ()=>({
+    isOpen: false
+  })
 }
 </script>
 
 <style>
-
+  .is-current {
+    font-weight: bold;
+    color: blueviolet
+  }
 </style>
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -13,6 +13,7 @@
       </h2>
       <p> My name is Megan, and I'm a Computer Science graduate student at SUNY New Paltz.</p>
       <p> For one of my classes, Web Server Programming, we are making a Fitness App. </p>
+      <p> If you have any questions or comments, feel free to send me an email :</p> <a href="mailto:schmidtm6@hawkmail.newpaltz.edu?subject=Fitness%20App%20Comment">schmidtm6@hawkmail.newpaltz.edu</a>
     </div>
   </div>
 </section>
