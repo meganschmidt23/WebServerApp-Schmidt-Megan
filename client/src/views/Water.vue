4468bdc9f39d3bfb0eb0414eec01e7675213dfdc
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-03-11 17:36:15 -0400

    Updated water methods

--- client/src/views/Water.vue
+++ client/src/views/Water.vue
@@ -2,32 +2,43 @@
     <section>
         <h1 class="title">Daily Water Intake</h1>
         <h2 class="subtitle"> Suggested intake is 75 ounces of water for 150 lb person.</h2>
-        <progress class="progress is-large" value="x" max="100">x%</progress>
+        <progress class="progress is-large" value="x" max="100"></progress>
         <div class="control">
-            <label class="radio">
-                <input type="radio" name="water">
-                    1 ounce
+            <label class="button">
+                <input type="submit" name="water" @click="plus1()" value="1 ounce">
                 </label>
-            <label class="radio">
-                <input type="radio" name="water">
-                    5 ounces
+            <label class="button">
+                <input type="submit" name="water" @click="plus5()" value="5 ounces">
                 </label>
-            <label class="radio">
-                <input type="radio" name="water">
-                    10 ounces
+            <label class="button">
+                <input type="submit" name="water" @click="plus10()" value="10 ounces">
                 </label>
-            <label class="radio">
-                <input type="radio" name="water">
-                    20 ounces
+            <label class="button">
+                <input type="submit" name="water" @click="plus20()" value= "20 ounces">
                 </label>
         </div>
-        <input class="button" type="submit" value="Submit input">
 
     </section>    
 </template>
 
 <script>
 export default {
-    
+    data: () => ({
+        x: 0
+    }),
+    methods: {
+        plus1(){
+            this.x += 1;
+        },
+        plus5(){
+            this.x +=5;
+        },
+        plus10(){
+            this.x+= 10;
+        },
+        plus20(){
+            this.x+=20
+        }
+    }
 }
 </script>
