3133f6d73161a435e3b75ec472488a45cd4343f6
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-03-17 21:43:31 -0400

    Corrected errors on Water screen

--- client/src/views/Water.vue
+++ client/src/views/Water.vue
@@ -1,23 +1,27 @@
 <template>
     <section>
         <h1 class="title">Daily Water Intake</h1>
-        <h2 class="subtitle"> Suggested intake is 75 ounces of water for 150 lb person.</h2>
-        <progress class="progress is-large" value="x" max="100"></progress>
-        <div class="control">
-            <label class="button">
-                <input type="submit" name="water" @click="plus1()" value="1 ounce">
-                </label>
-            <label class="button">
-                <input type="submit" name="water" @click="plus5()" value="5 ounces">
-                </label>
-            <label class="button">
-                <input type="submit" name="water" @click="plus10()" value="10 ounces">
-                </label>
-            <label class="button">
-                <input type="submit" name="water" @click="plus20()" value= "20 ounces">
-                </label>
-        </div>
 
+        <h2 class="subtitle"> Suggested intake is 75 ounces of water for 150 lb person. </h2>
+        <figure class="image is-128x128">
+           <img src="https://s3.envato.com/files/250136728/IMG_9444.jpg">
+        </figure>   
+        <progress class="progress is-large" :value="x" max="75"></progress>
+            <h1>Total: {{x}} ounces </h1>
+                <div class="control">
+                     <label class="button">
+                        <input type="submit" name="water" value="1 ounce" @click="plus1()" >
+                    </label>
+                     <label class="button">
+                         <input type="submit" name="water" value="5 ounces" @click="plus5()" >
+                     </label>
+                    <label class="button">
+                        <input type="submit" name="water" value="10 ounces" @click="plus10()" >
+                     </label>
+                    <label class="button">
+                         <input type="submit" name="water" value= "20 ounces" @click="plus20()" >
+                    </label>
+                 </div>
     </section>    
 </template>
 
